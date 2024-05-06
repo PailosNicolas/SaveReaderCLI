0036e1900b459a8b4a036808b361914b98bf0aa8
PailosNicolas <Pailosnicolas@gmail.com>
2024-05-06 01:40:17 +0000

    Added bubbles menu non functional

--- mainMenu/main_menu.go
+++ mainMenu/main_menu.go
@@ -2,7 +2,9 @@ package main_menu
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/charmbracelet/bubbles/filepicker"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -12,6 +14,7 @@ type model struct {
 	readSaveChoices []choices // items on the to-do list
 	cursor          int       // which to-do list item our cursor is pointing at
 	selectedCode    string
+	filePicker      filepicker.Model
 }
 
 type choices struct {
@@ -20,12 +23,15 @@ type choices struct {
 }
 
 func InitialModel() model {
+	fp := filepicker.New()
+	fp.AllowedTypes = []string{".sav", ".pkmn"}
+	fp.CurrentDirectory, _ = os.Getwd()
 	return model{
 		// Our to-do list is a grocery list
 		mainMenuChoices: []choices{{name: "Read save", code: "read_save"}, {name: "Load pokemon", code: "load_pokemon"}},
 		readSaveChoices: []choices{{name: "Read file", code: "read_file"}, {name: "Go to main menu", code: "main_menu"}},
 		choices:         []choices{{name: "Read save", code: "read_save"}, {name: "Load pokemon", code: "load_pokemon"}},
-
+		filePicker:      fp,
 		// A map which indicates which choices are selected. We're using
 		// the  map like a mathematical set. The keys refer to the indexes
 		// of the `choices` slice, above.
@@ -34,8 +40,7 @@ func InitialModel() model {
 }
 
 func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
-	return nil
+	return m.filePicker.Init()
 }
 
 func (m model) View() string {
@@ -77,49 +82,67 @@ func (m model) View() string {
 		// The footer
 		s += "\nPress q to quit.\n"
 
+	case "read_file":
+		s += m.filePicker.View()
+
 	}
 	return s
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
 	switch msg := msg.(type) {
 
 	// Is it a key press?
 	case tea.KeyMsg:
 
-		// Cool, what was the actual key pressed?
-		switch msg.String() {
-
-		// These keys should exit the program.
-		case "ctrl+c", "q":
-			return m, tea.Quit
-
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
+		switch m.selectedCode {
+		case "read_file":
+			switch msg.String() {
+			case tea.KeyEsc.String():
+				m.selectedCode = "read_save"
+				return m, nil
 			}
+			m.filePicker, cmd = m.filePicker.Update(msg)
+			return m, cmd
+		default:
+			// Cool, what was the actual key pressed?
+			switch msg.String() {
 
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
+			// These keys should exit the program.
+			case "ctrl+c", "q":
+				return m, tea.Quit
 
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			m.selectedCode = m.choices[m.cursor].code
-			switch m.selectedCode {
-			case "main_menu":
-				m.choices = m.mainMenuChoices
-			case "read_save":
-				m.choices = m.readSaveChoices
+			// The "up" and "k" keys move the cursor up
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+
+			// The "down" and "j" keys move the cursor down
+			case "down", "j":
+				if m.cursor < len(m.choices)-1 {
+					m.cursor++
+				}
+
+			// The "enter" key and the spacebar (a literal space) toggle
+			// the selected state for the item that the cursor is pointing at.
+			case "enter", " ":
+				m.selectedCode = m.choices[m.cursor].code
+				switch m.selectedCode {
+				case "main_menu":
+					m.choices = m.mainMenuChoices
+				case "read_save":
+					m.choices = m.readSaveChoices
+				}
 			}
+			return m, nil
 		}
 	}
 
+	m.filePicker, cmd = m.filePicker.Update(msg)
+
 	// Return the updated model to the Bubble Tea runtime for processing.
 	// Note that we're not returning a command.
-	return m, nil
+	return m, cmd
 }
