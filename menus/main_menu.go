9ab2e5e0ee23f46569d7087fb8b6f7f2f80cdb86
PailosNicolas <Pailosnicolas@gmail.com>
2024-05-18 17:33:29 +0000

    Cleaned up leftovers in main_menu

--- menus/main_menu.go
+++ menus/main_menu.go
@@ -125,27 +125,21 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.filePicker, cmd = m.filePicker.Update(msg)
 			return m, cmd
 		default:
-			// Cool, what was the actual key pressed?
 			switch msg.String() {
 
-			// These keys should exit the program.
 			case "ctrl+c", "q":
 				return m, tea.Quit
 
-			// The "up" and "k" keys move the cursor up
 			case "up", "k":
 				if m.cursor > 0 {
 					m.cursor--
 				}
 
-			// The "down" and "j" keys move the cursor down
 			case "down", "j":
 				if m.cursor < len(m.choices)-1 {
 					m.cursor++
 				}
 
-			// The "enter" key and the spacebar (a literal space) toggle
-			// the selected state for the item that the cursor is pointing at.
 			case "enter", " ":
 				m.selectedCode = m.choices[m.cursor].code
 				switch m.selectedCode {
@@ -161,7 +155,5 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	m.filePicker, cmd = m.filePicker.Update(msg)
 
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
 	return m, cmd
 }
